167d4f60631d29f3c129af229d8882aa9b34fbd4
Roc Wu <rocwind@gmail.com>
2020-03-30 13:04:10 +0800

    chore: update dependencies

--- src/web/web.ts
+++ src/web/web.ts
@@ -4,9 +4,10 @@ applyPolyfill(); // need to force applyPolyfill for ios issue
 
 import { Endpoint } from 'comlinkjs';
 import { wrap } from 'comlinkjs/messagechanneladapter';
-import { beginWait } from 'wait-ready';
+import { wait } from 'wait-ready';
 
-const { wait, getStatus, setReady, setFailed } = beginWait();
+const { afterReady: waitEndpointReady, getStatus, setReady, setFailed } = wait<void>();
+
 /**
  * check if Comlink endpoint is ready for sending & received message
  */
@@ -14,7 +15,7 @@ export const getEndpointStatus = getStatus;
 /**
  * wait for endpont ready, returns a promise that resolve or reject on endpoint ready/failed
  */
-export const waitEndpointReady = wait;
+export { waitEndpointReady };
 
 const ENDPOINT_CHECK_INTERVAL: number = 200;
 const ENDPOINT_CHECK_TIMEOUT: number = 10000;
@@ -68,6 +69,6 @@ export function createEndpoint(): Endpoint {
         dispatchEvent(event) {
             window.dispatchEvent(event);
             return document.dispatchEvent(event);
-        }
+        },
     });
 }
