2b88f300864ae656823efe9b70423a3544c917f7
Roc Wu <rocwind@gmail.com>
2021-08-10 15:18:46 +0800

    fix: compatible with Android 5 browser

--- src/common/uuid.ts
+++ src/common/uuid.ts
@@ -1,7 +1,6 @@
 // random UUID
 export function generateUUID(): string {
-    return new Array(4)
-        .fill(0)
+    return [0, 0, 0, 0]
         .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
         .join('-');
 }
--- src/web/web.ts
+++ src/web/web.ts
@@ -1,3 +1,4 @@
+import './polyfill';
 import { JavascriptInterface, MessageEvent } from './types';
 import { MessageHub } from '../common/MessageHub';
 import { createTransmitter } from '../common/message';
@@ -14,22 +15,33 @@ const logger = createLogger($LOG_ENABLED);
  */
 function createInterface<T>(name: string, target: T, os: string): JavascriptInterface<T> {
     const hub = new MessageHub(name, window.ReactNativeWebView);
-    const listener = (evt: MessageEvent) => {
-        const msg = evt.data;
-        if (hub.canHandleMessage(msg)) {
-            logger(`received message from native ${msg}`);
-            hub.handleMessage(msg);
+    let initialized = false;
+    const init = () => {
+        if (initialized) {
+            return;
+        }
+        initialized = true;
+        const listener = (evt: MessageEvent) => {
+            const msg = evt.data;
+            if (hub.canHandleMessage(msg)) {
+                logger(`received message from native ${msg}`);
+                hub.handleMessage(msg);
+            }
+        };
+
+        if (os === 'android') {
+            document.addEventListener('message', listener);
+        } else if (os === 'ios') {
+            window.addEventListener('message', listener);
         }
     };
 
-    if (os === 'android') {
-        document.addEventListener('message', listener);
-    } else if (os === 'ios') {
-        window.addEventListener('message', listener);
-    }
-
     return Object.keys(target).reduce((obj, key) => {
-        obj[key] = createTransmitter(key, hub);
+        const transmitter = createTransmitter(key, hub);
+        obj[key] = (...args: unknown[]) => {
+            init();
+            return transmitter(...args);
+        };
         return obj;
     }, {} as JavascriptInterface<T>);
 }
